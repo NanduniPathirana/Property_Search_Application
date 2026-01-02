c1fcd11723e827f345c5cdff81c1fd1d28689fe1
Nanduni Pathirana <nandunipathirana2003@gmail.com>
2026-01-02 16:48:45 +0530

    Add HTML5 property search form UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+//Main react component for the property search application
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    //Main container for the search pages
+    <div className="app">
+      <h1>Believe in Finding</h1>
+      <h2>with the SL's largest choice of properties </h2>
+
+      <p>
+        Search properties to buy
       </p>
-    </>
+
+      {/* Collect user inputs to seearch properties*/}
+      <form className="search-form">
+
+        {/* Allows user to specify the type of property */}
+        <div className="search-group">
+          <label htmlFor="propertyType">
+            Property Type
+          </label>
+
+          <select id="propertyType" name="propertyType">
+            <option value="house">House</option>
+            <option value="flay">Flat</option>
+            <option value="any">Any</option>
+          </select>
+        </div>
+
+        {/* Allows user to specify the price range */}
+        <div className="search-group">
+          {/* Minimum price input */}
+          <label htmlFor="minPrice">Minimum Price</label>
+          <input
+             type="number"
+             id="minPrice"
+             name="minPrice"
+             placeholder="Min Price"
+          />
+
+          {/* Maximum price input */}
+          <label htmlFor="maxPrice">Maximum Price</label>
+          <input
+             type="number"
+             id="maxPrice"
+             name="maxPrice"
+             placeholder="Max Price"
+          />
+        </div>
+
+        {/* Allows user to specify the number of bedrooms */}
+        <div className="search-group">
+          {/* Minimum bedrooms input */}
+          <label htmlFor="minBedrooms">Minimum Bedrooms</label>
+          <input 
+             type="number"
+             id="minBedrooms"
+             name="minBedrooms"
+             placeholder="Min Bedrooms"
+          />
+
+          {/* Maximum bedrooms input */}
+          <label htmlFor="maxBedrooms">Maximum Bedrooms</label>
+          <input 
+             type="number"
+             id="maxBedrooms"
+             name="maxBedrooms"
+             placeholder="Max Bedrooms"
+          />
+        </div>
+
+        {/* Allows users to search properties added after a specific date */}
+        <div className="search-group">
+          {/* Added after a specific date */}
+          <label htmlFor="dateAdded">Date Added</label>
+          <input
+             type="date"
+             id="dateAdded"
+             name="dateAdded"
+          />
+        </div>
+
+        {/* Allows users to enter the 1st part of the postcode */}
+        <div className="search-group">
+          <label htmlFor="postCode">Postcode</label>
+          <input
+             type="text"
+             id="postcode"
+             name="postcode"
+             placeholder="e.g.-BR1, NW1"
+          />
+        </div>
+
+
+      </form>
+
+    </div>
   )
 }
 
-export default App
+export default App;
